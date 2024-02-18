43940e37f28d5dd921044473e94a6c827441bdbe
Eryhnar <pedrofernbel@gmail.com>
2024-02-18 19:43:44 +0100

    Done Reto 9.

---
+++ reto9/js/main.js
@@ -0,0 +1,34 @@
+
+let size
+
+let randomArray = []
+
+let total = 0
+
+minNum = 0
+maxNum = 9
+
+while (!parseInt(size)) 
+{
+  size = prompt("Enter the size of the board: ")
+}
+
+const fillArray = (minNum, maxNum, size) => 
+{
+  for (let i = 0; i < size; i++) 
+  {
+    randomArray.push(Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum)
+  }
+}
+
+const printArray = (array) =>
+{
+  randomArray.map(item => //should use .reduce
+  {
+    total += item
+  })
+  console.log(randomArray, " total: ", total)
+}
+
+fillArray(minNum, maxNum, size)
+printArray(randomArray)
